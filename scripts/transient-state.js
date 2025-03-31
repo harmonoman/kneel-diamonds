d2880842af0cb7730a24136da07c4294fe27ff90
Mark Bond <markbond615@gmail.com>
2025-03-30 20:39:46 -0500

    Added a CustomEvent for when an order is placedto display orders

--- scripts/transient-state.js
+++ scripts/transient-state.js
@@ -1,45 +1,37 @@
 const transientState = {
-    metal: 0,
-    size: 0,
-    style: 0
+    metalId: 0,
+    sizeId: 0,
+    styleId: 0
 }
 
 export const setMetalChoice = (metalChoice) => {
-    transientState.metal = metalChoice;
-    console.log("transient state: ", transientState);
+    transientState.metalId = metalChoice;
 }
 
 export const setSizeChoice = (sizeChoice) => {
-    transientState.size = sizeChoice;
-    console.log("transient state: ", transientState);
-
+    transientState.sizeId = sizeChoice;
 }
 
 export const setStyleChoice = (styleChoice) => {
-    transientState.style = styleChoice;
-    console.log("transient state: ", transientState);
-
+    transientState.styleId = styleChoice;
 }
 
 export const resetTransientState = () => {
-    transientState.metal = 0;
-    transientState.size = 0;
-    transientState.style = 0;
+    transientState.metalId = 0;
+    transientState.sizeId = 0;
+    transientState.styleId = 0;
 }
 
 export const placeOrder = async () => {
-    /*
-        Add the required keys to the object below that are needed for a POST operation.
-    */
     const postOptions = {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(transientState)
     }
 
-    // Send the transient state to your API
     const response = await fetch("http://localhost:8088/orders", postOptions);
-    console.log("response: ", response)
 
     resetTransientState();
+
+    document.dispatchEvent(new CustomEvent("newOrderPlaced"));
 }
